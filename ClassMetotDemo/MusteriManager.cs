e4cf4381d1639512177f895d398c2cb8ff27a861
Enver Can Çınardal <eccinardal1907@gmail.com>
2021-05-03 22:04:47 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager //ekleme - listeleme - silme
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listesi : " + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Ad = "Enver";
+            musteri.Soyad = "Can";
+            musteri.Yas = 20;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Ali";
+            musteri2.Yas = 21;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Hasan";
+            musteri3.Soyad = "Ali";
+            musteri3.Yas = 22;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("-------------------------------");
+
+            musteriManager.Listele(musteri);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+            Console.WriteLine("-------------------------------");
+
+            musteriManager.Sil(musteri);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sil(musteri3);
+        }
+    }
+}
